066ec3d28946ed2760004e9d3ca824aae17d805d
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-06-19 10:23:26 +0200

    temp: vite-plugin-astro

--- examples/netlify-hybrid/vite.config.ts
+++ examples/netlify-hybrid/vite.config.ts
@@ -1,5 +1,6 @@
 import netlify from "@vite-deploy/netlify";
 import { defineConfig } from "vite";
+import astro from "vite-plugin-astro";
 
 export default defineConfig({
 	plugins: [
@@ -10,5 +11,42 @@ export default defineConfig({
 			},
 			handlerEntrypoint: "./src/handler.ts",
 		}),
+		astro({
+			transformOptions: {
+				internalURL: "virtual:temp",
+			},
+		}),
+		{
+			name: "temp",
+			resolveId: {
+				filter: { id: new RegExp(`^${"virtual:temp"}$`) },
+				handler() {
+					return "\0virtual:temp";
+				},
+			},
+			load: {
+				filter: {
+					id: new RegExp(`^${"\0virtual:temp"}$`),
+				},
+				handler() {
+					return `
+						export function render() {}
+						export function createComponent(cb) {
+							return cb
+						}
+						export function maybeRenderHead() {}
+					`;
+				},
+			},
+			transform: {
+				filter: {
+					id: /\.astro$/,
+				},
+				handler(code) {
+					console.log({ code });
+					return code;
+				},
+			},
+		},
 	],
 });
